5e2653f1805e33452f1e89fd8311166c431324c9
faaiq-khan <faiq.khan@techwards.co>
2021-12-18 17:12:28 +0500

    checkout api updated

--- daily_deals/lib/modals/checkout_modal.dart
+++ daily_deals/lib/modals/checkout_modal.dart
@@ -1,6 +1,6 @@
 class CheckoutModal {
-  String userId, email, phone;
-  String? firstName, zip, language, lastName, address, city, state;
+  String userId, email, phone, firstName, addressType;
+  String? zip, language, lastName, address, city, state;
   String total;
   List<CheckoutItemModal> items;
 
@@ -8,28 +8,43 @@ class CheckoutModal {
     this.userId,
     this.email,
     this.phone,
+    this.firstName,
     this.total,
-    this.items,);
+    this.items,
+    this.addressType,
+  );
 
   Map<String, dynamic> toJson() => _$CheckoutModalToJson(this);
 }
 
-Map<String, dynamic> _$CheckoutModalToJson(CheckoutModal instance) => <String, dynamic> {
-  "userid": instance.userId,
-  "email": instance.email,
-  "phone": instance.phone,
-  "firstname": "faiq",
-  "lastname": "khan",
-  "total": instance.total,
-  "language": "en",
-  "items": instance.items.map((item) => item.toJson()).toList(),
-};
+Map<String, dynamic> _$CheckoutModalToJson(CheckoutModal instance) =>
+    <String, dynamic>{
+      "userid": instance.userId,
+      "email": instance.email,
+      "phone": instance.phone,
+      "firstname": instance.firstName,
+      "lastname": "khan",
+      "address": instance.address ?? "",
+      "city": instance.city ?? "",
+      "state": instance.state ?? "",
+      "zip": instance.zip ?? "",
+      "total": instance.total,
+      "language": "en",
+      "address_type": instance.addressType,
+      "items": instance.items.map((item) => item.toJson()).toList(),
+    };
 
 class CheckoutItemModal {
-  String id, quantity;
-  String subTotal;
+  String id, quantity, subTotal, deliveryType;
+  List<dynamic> sequence;
 
-  CheckoutItemModal(this.id, this.quantity, this.subTotal);
+  CheckoutItemModal(
+    this.id,
+    this.quantity,
+    this.subTotal,
+    this.deliveryType,
+    this.sequence,
+  );
 
   Map<dynamic, dynamic> toJson() => _$itemToJson(this);
 }
@@ -39,4 +54,6 @@ Map<dynamic, dynamic> _$itemToJson(CheckoutItemModal instance) =>
       "id": instance.id,
       "qty": instance.quantity,
       "subtotal": instance.subTotal,
+      "delivery_type": instance.deliveryType,
+      "code_sequence": instance.sequence,
     };
--- daily_deals/lib/screens/cart_screen.dart
+++ daily_deals/lib/screens/cart_screen.dart
@@ -5,6 +5,7 @@ import 'package:daily_deals/providers/cart_item_provider.dart';
 import 'package:daily_deals/screens/order_confirmation_screen.dart';
 import 'package:daily_deals/screens/parent_screen.dart';
 import 'package:daily_deals/service/webservice.dart';
+import 'package:daily_deals/utils/constants.dart';
 import 'package:daily_deals/utils/utils.dart';
 import 'package:daily_deals/utils/widget_utils.dart';
 import 'package:daily_deals/views/cart_address_details_view.dart';
@@ -18,6 +19,7 @@ import 'package:hexcolor/hexcolor.dart';
 import 'package:hive/hive.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import '../utils/constants.dart';
 
 // ignore: must_be_immutable
 class CartScreen extends StatelessWidget {
@@ -249,15 +251,21 @@ class CartScreen extends StatelessWidget {
                         item.productId,
                         item.itemCount.toString(),
                         item.price,
+                        isAddressRequired.containsValue(0)
+                            ? "Donate"
+                            : "Normal",
+                        item.mySequence.values.toList(),
                       ),
                     );
                   }
                   CheckoutModal checkoutItem = CheckoutModal(
-                    preferences.getString("user_id")!,
-                    preferences.getString("email")!,
-                    preferences.getString("phoneNumber")!,
+                    preferences.getString(Constants.USER_ID)!,
+                    preferences.getString(Constants.EMAIL)!,
+                    preferences.getString(Constants.PHONE_NUMBER)!,
+                    preferences.getString(Constants.USER_NAME)!,
                     totalPrice.toString(),
                     checkoutItems,
+                    "Work",
                   );
                   List<dynamic> response =
                       await WebService.checkoutProduct(checkoutItem);
